7d634b9bdc1bf660a740bcea480c74a8df858551
Catarina Silva e Meirelles <catlls622@gmail.com>
2025-09-17 11:44:12 +0000

    BuscaBinariaStrings

--- TRABALHOS/tradutorMorse.c
+++ TRABALHOS/tradutorMorse.c
@@ -3,6 +3,7 @@
 
 // funçao de menu
 //funçao de recebimento/tratamento dos dados(guardar em uma matriz cada palavra(linha) letra(coluna))
+// o maior tamanho que uma letra em morse tem é 4+\0 = 5
 
 #include <stdio.h>
 #include <string.h>
