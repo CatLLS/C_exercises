f949e29a72536ac93fe8226ad05a0904fe42b15b
Catarina Silva e Meirelles <catlls622@gmail.com>
2025-09-27 00:25:25 +0000

    menuTrabalho

--- TRABALHOS/tradutorMorse.c
+++ TRABALHOS/tradutorMorse.c
@@ -62,20 +62,50 @@ void tratarInput(int tamanhoMSG, char msgMatriz[][5]){
     
 }
 
+char resolverCorrompimento(){ //FAZER (helô/Lívia!!!)
+    //retornar letras possíveis (?)
+}
+
+void decodificar(){//FAZER (helô/Lívia!!!)
+    resolverCorrompimento();
+    //msgDecodificada[]=
+    
+    //alterar a matriz mensagem alfabeto! não precisa retornar nada, favor não printar nada nessas funções
+}
+
 int main(){
     char alfabetoMorse[][5] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--", "-.","---",".--.","--.-", ".-.", "...","-","..-","...-",".--","-..-","-.--","--.."};
     char alfabeto[]= {'A','B','C','D','E', 'F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
     
     int tamanhoMSG=300;
     char msgTratada[tamanhoMSG/5][5];
-    
-    tratarInput(tamanhoMSG, msgTratada);
-    //para debug(o for abaixo printa cada letra da mensagem tratada, podem usar se precisarem meninas!), deletar depois:
-    /*for (int i=0; i< (tamanhoMSG/5) && msgTratada[i][0] != '\0'; i++){
-        printf("\nPalavra %d:>%s<", i+1, msgTratada[i]);
-    }
-    */
+    char msgDecodificada[tamanhoMSG/5];
+    char deNovo[2];
 
-    printf("\nProgama encerrado com sucesso.\n");
+    do{
+        printf("\n*⣾⠛⠛⠲⣤           ⣠⡴⠞⠛⢻⡆*\n*⣿      ⠙⢦⡀    ⣠⠞⠁   ⢸⡇ ⁺⊹*\n*⢿⡄        ⠹⣄⡼⠁      ⣼⠇*\n*⠘⢷⣄⣀⡀.    ⠙⠁     ⣀⣀⣴⠟*\n  *⣼⠛⠉⠉          ⠈⠉⠙⢻⡄*\n *⠘⣷        ⡞⣦     ⣰⡟*\n*⊹ ⠈⠻⣦⣤⡶⠋⠈   ⠳⣦⣤⡾⠋   ₊*");
+        printf("\n✩₊˚.⋆☾⋆⁺₊✧ Bem vindo ao Decodificador Morse 3000 ✩₊˚.⋆☾⋆⁺₊✧ \n\n-resolvendo códigos desde a era da guerra");
+        printf("\n--------🗝️⋆.⚚.⋆꩜.⚚ᐟ⋆.🗝️--------\n");
+
+        tratarInput(tamanhoMSG, msgTratada);
+
+        decodificar(tamanhoMSG, msgTratada, msgDecodificada);//Obs: a função resolver corrompimento está dentro da função decodificar!!!
+
+        //para debug(o for abaixo printa cada letra da mensagem tratada, podem usar se precisarem meninas!), deletar depois:
+        /*for (int i=0; i< (tamanhoMSG/5) && msgTratada[i][0] != '\0'; i++){
+            printf("\nPalavra %d:>%s<", i+1, msgTratada[i]);
+        }*/
+        //inserir gato
+        printf("\nHmm, os nossos detetives chegaram a conclusão de que a sua mensagem significa: \n");
+        printf("\n%s\n", msgDecodificada);
+        printf("\n                  ╱|、                   \n                (˚ˎ 。7  \n                 |、˜〵          \n                じしˍ,)ノ");
+        
+        printf("Deseja decodificar outra mensagem?(s/n)");
+        fgets(deNovo,1,stdin);
+
+    }while(deNovo == "S");
+    
+    printf("\nProgama encerrado com sucesso, Obrigada por usar o Decodificador Morse 3000!\n\n ∧,,,∧\n( ̳• ·• ̳)\n/   づ♡ ");
+
     return 0;
 }
