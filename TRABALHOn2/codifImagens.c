60bea23d835158096c192139be1c7a090f8d1407
Catarina Silva e Meirelles <catlls622@gmail.com>
2025-11-12 00:07:30 +0000

    terminei o algoritmo de corte e analise N2

--- TRABALHOn2/codifImagens.c
+++ TRABALHOn2/codifImagens.c
@@ -6,8 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Assinatura da codificar_imagem-vamos precisar dela 
+void codificar_imagem(int altura, int largura, int matriz[altura][largura], char resultado[]);
+
 void printImg(int altura, int largura, int matriz[altura][largura]){
-   for(int i = 0; i < altura; i++){
+   printf("\n----");
+    for(int i = 0; i < altura; i++){
         printf("\n");
         for(int j = 0; j < largura; j++){ 
             printf(" %d", matriz[i][j]);
@@ -15,19 +19,67 @@ void printImg(int altura, int largura, int matriz[altura][largura]){
    }
 }
 
-void cortar(int altura, int largura, int matriz[altura][largura]){
-    int corteX2 = altura/2;//divisão inteira
-    int corteX1 = altura-corteX2;
-    int corteY2 = largura/2;
-    int corteY1 = largura-corteY2;
-    printf("\n<%d><%d><%d><%d>\n", corteX1, corteX2, corteY1, corteY2);
+void copiarMatriz(  int tamanhoCopiaX,
+                    int tamanhoCopiaY, 
+                    int copia[tamanhoCopiaX][tamanhoCopiaY], 
+                    int tamanhoOriginalX,
+                    int tamanhoOriginalY, 
+                    int original[tamanhoOriginalX][tamanhoOriginalY],
+                    int inicioX, int fimX, int inicioY, int fimY){
+    
+    for(int i= 0; i< tamanhoCopiaX; i++){
+        for(int j=0; j<tamanhoCopiaY; j++){
+            copia[i][j] = original[i+inicioX][j+inicioY];
+        }
+    }
+}
+
+void cortar(int altura, int largura, int matriz[altura][largura], char resultado[]){
+    int X2 = altura/2;//divisão inteira
+    int X1 = altura-X2;
+    int Y2 = largura/2;
+    int Y1 = largura-Y2;
+    printf("\n<%d><%d><%d><%d>\n", X1, X2, Y1, Y2);
+    if(X1 != 0){
+        if(Y1 != 0){
+            int md1[X1][Y1];
+            copiarMatriz(X1,Y1, md1, altura, largura, matriz, 0,X1,0,Y1);
+            printImg(X1,Y1,md1);
+            codificar_imagem(X1,Y1, md1, resultado);
+        }
+        if(Y2 != 0){
+            int md2[X1][Y2];
+            copiarMatriz(X1,Y2, md2, altura,largura, matriz, 0,X1,Y1,largura);
+            printImg(X1,Y2,md2);
+            codificar_imagem(X1,Y2, md2, resultado);
+        }
+    }
+    if(X2 != 0){
+        if(Y1 != 0){
+            int md3[X2][Y1];
+            copiarMatriz(X2,Y1, md3, altura,largura, matriz, X1,altura,0,Y1);
+            printImg(X2,Y1,md3);
+            codificar_imagem(X2,Y1, md3, resultado);
+        }
+        if(Y2 != 0){
+            int md4[X2][Y2];
+            copiarMatriz(X2,Y2, md4, altura,largura, matriz, X1,altura,Y1,largura);
+            printImg(X2,Y2,md4);
+            codificar_imagem(X2,Y2, md4, resultado);
+        }
+    }
+    // x qtde linhas, y qtde colunas
+    // m1 = matriz de 0 até(<) X1 e Y1, tamanho [X1][Y1]
+    // m2 = matriz de 0 até X2 e Y1 até largura, se X2 !=0, tamanho[X1][Y2]
+    // m3 = matriz de X1 até altura e de 0 até Y1, se , tamanho[X2][Y1]
+    // m4 = matriz de X1 até altura e Y2 até largura, tamanho[X2][Y2]
 }
 
 void codificar_imagem(int altura, int largura, int matriz[altura][largura], char resultado[]){//cat
     //analisar se a imagem inteira está completa, se não, cortá-la
     char preenchimento[2];
     int referencia = matriz[altura-1][largura-1];
-    printf("\nreferencia =<%d>\n", referencia);
+    //printf("\nreferencia =<%d>\n", referencia);
 
     if (referencia == 0){
             preenchimento[0] = 'B';
@@ -37,18 +89,18 @@ void codificar_imagem(int altura, int largura, int matriz[altura][largura], char
 
     if (altura <= 1 && largura <= 1){
         strcat(resultado, preenchimento);
-        printf("\nAltura atual: %d, Larg: %d, item[]: <%d>\n", altura, largura, matriz[altura-1][largura-1]);
+        //printf("\nAltura atual: %d, Larg: %d, item[]: <%d>\n", altura, largura, matriz[altura-1][largura-1]);
         return;
     }
 
     for(int i=0; i<altura; i++){
         for(int j=0; j<largura; j++){
-            printf("\nmatriz[%d][%d]= <%d>\n", i,j, matriz[i][j]);
+            //printf("\nmatriz[%d][%d]= <%d>\n", i,j, matriz[i][j]);
             if (matriz[i][j] != referencia){
                 preenchimento[0] = 'X';
                 strcat(resultado, preenchimento);
                 printf("\ncorte\n");
-                cortar(altura, largura, matriz);
+                cortar(altura, largura, matriz, resultado);
                 return;
             }
         }
@@ -88,28 +140,6 @@ void modo_manual(char resultado[]){//Livia
     return;
 }
 
-/*
-int modo_arquivo(char *file, char resultado[]){//bea
-    int largura, altura;
-    int matriz[altura][largura];
-    FILE *file;     
-
-    arqv = fopen(file[1], "r");
-
-    if(arqv == NULL){
-        printf("Erro ao abrir o arquivo. Arquivo vazio.");
-        return 1;
-    }
-
-    for(int i = 0; i <= altura; i++){
-        for(int j = 0; j <= largura; j++){
-            fscanf(file, "%d", &matriz[i][j]);
-        }
-    }
-    fclose(file);
-    codificar_imagem(altura, largura, matriz, &resultado);
-}*/
-
 int exibir_ajuda(){
     printf("Uso: ImageEncoder [-? | -m | -f ARQ]\n");
     printf("Codifica imagens binários dadas em arquivos PBM ou por dados informados pelo usuário.\n\n");
@@ -141,5 +171,7 @@ int main(int argc, char *argv[]){
         printf("Argumento inválido. Use -? ou --help para ver as opções disponíveis.\n");
         return 1;
     }
+
+    printf("\n--->O resultado final é: %s\n\n", resultado);
     return 0;
 }
